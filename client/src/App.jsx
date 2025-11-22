de1117ec49b532dbf7774c756e31b1f4b3695d70
devlopersumit <sumitsj2002@gmail.com>
2025-11-22 10:59:21 +0530

    Setup StockMaster Folder Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-blue-700">
+    Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
